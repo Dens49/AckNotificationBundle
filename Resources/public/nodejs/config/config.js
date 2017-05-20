aca8dce0b881af3ea3e498a4952afeba2960e296
Dennis Bystrow <dennis.bystrow@gmail.com>
2017-05-20 20:24:00 +0200

    allow usage of custom config.json

--- Resources/public/nodejs/config/config.js
+++ Resources/public/nodejs/config/config.js
@@ -1,2 +1,14 @@
-const nconf = require('nconf');
-module.exports = nconf.file(__dirname + '/config.json');
+const nconf = require('nconf'),
+      fs    = require('fs');
+
+const user_config_path = __dirname + '/../../../app/js/acknotification/config.json';
+const bundle_config_path = __dirname + '/config.json';
+var config = null;
+
+if (fs.existsSync(user_config_path)) {
+    config = nconf.file(user_config_path);
+}
+else {
+    config = nconf.file(bundle_config_path);
+}
+module.exports = config;
